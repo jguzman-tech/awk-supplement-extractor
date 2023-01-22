60a1056f966ba872e5e8979065f3a26fcabe3144
Joseph Guzman <51888667+jguzman-tech@users.noreply.github.com>
2023-01-21 19:42:14 -0700

    Update extract-code.sh

--- extract-code.sh
+++ extract-code.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# Description:
-
-# Extract files from awkcode.txt (provided here:
-# https://www.cs.princeton.edu/~bwk/btl.mirror/awkcode.txt), which is a
-# supplementary material provided by one of the authors of "The Awk Programming
-# Language". This script is helpful because it gives you a TUI menu for
-# selecting an example among the 236 examples provided in that one awkcode.txt
-# resource.
-
-# Usage:
-
-# The script takes no arguments, simply navigate the menu, when you pick a file
-# and confirm with 'y' it will write that file to your current working
-# directory. The script assumes you have downloaded awkcode.txt and that it
-# exists in the same directory as the script itself.
-
 function extract() {
     local file
     file='examples.index'
